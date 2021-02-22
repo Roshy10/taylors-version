e4b9f45361b67bbc1c7ed359216a227e321014bd
Roshan Bhumbra <rbhumbra@gmail.com>
2021-02-22 18:38:18 +0000

    auto staging deploy on push to develop

---
+++ cdk/bin/cdk.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import cdk = require('@aws-cdk/core');
+import {StaticSite} from '../lib/cdk-stack';
+
+/**
+ * This stack relies on getting the domain name from CDK context.
+ * Use 'cdk synth -c domain=mystaticsite.com -c subdomain=www'
+ * Or add the following to cdk.json:
+ * {
+ *   "context": {
+ *     "domain": "mystaticsite.com",
+ *     "subdomain": "www"
+ *   }
+ * }
+ **/
+class MyStaticSiteStack extends cdk.Stack {
+    constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
+        super(parent, name, props);
+
+        new StaticSite(this, 'StaticSite', {
+            domainName: this.node.tryGetContext('domain'),
+        });
+    }
+}
+
+const app = new cdk.App();
+
+new MyStaticSiteStack(app, 'MyStaticSite', {
+    env: {
+        // Stack must be in us-east-1, because the ACM certificate for a
+        // global CloudFront distribution must be requested in us-east-1.
+        region: 'us-east-1'
+    }
+});
+
+app.synth();
---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+import cloudfront = require('@aws-cdk/aws-cloudfront');
+import route53 = require('@aws-cdk/aws-route53');
+import s3 = require('@aws-cdk/aws-s3');
+import s3deploy = require('@aws-cdk/aws-s3-deployment');
+import acm = require('@aws-cdk/aws-certificatemanager');
+import cdk = require('@aws-cdk/core');
+import targets = require('@aws-cdk/aws-route53-targets/lib');
+import {Construct} from '@aws-cdk/core';
+
+export interface StaticSiteProps {
+    domainName: string;
+}
+
+/**
+ * Static site infrastructure, which deploys site content to an S3 bucket.
+ *
+ * The site redirects from HTTP to HTTPS, using a CloudFront distribution,
+ * Route53 alias record, and ACM certificate.
+ *
+ * copied from https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/static-site/static-site.ts
+ */
+export class StaticSite extends Construct {
+    constructor(parent: Construct, name: string, props: StaticSiteProps) {
+        super(parent, name);
+
+        const zone = route53.HostedZone.fromLookup(this, 'Zone', {domainName: props.domainName});
+        const siteDomain = props.domainName;
+        new cdk.CfnOutput(this, 'Site', {value: 'https://' + siteDomain});
+
+        // Content bucket
+        const siteBucket = new s3.Bucket(this, 'SiteBucket', {
+            bucketName: siteDomain,
+            websiteIndexDocument: 'index.html',
+            publicReadAccess: true,
+        });
+        new cdk.CfnOutput(this, 'Bucket', {value: siteBucket.bucketName});
+
+        // TLS certificate
+        const certificateArn = new acm.DnsValidatedCertificate(this, 'SiteCertificate', {
+            domainName: siteDomain,
+            hostedZone: zone,
+            region: 'us-east-1', // Cloudfront only checks this region for certificates.
+        }).certificateArn;
+        new cdk.CfnOutput(this, 'Certificate', {value: certificateArn});
+
+        // CloudFront distribution that provides HTTPS
+        const distribution = new cloudfront.CloudFrontWebDistribution(this, 'SiteDistribution', {
+            aliasConfiguration: {
+                acmCertRef: certificateArn,
+                names: [siteDomain],
+                sslMethod: cloudfront.SSLMethod.SNI,
+                securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_1_2016,
+            },
+            originConfigs: [
+                {
+                    customOriginSource: {
+                        domainName: siteBucket.bucketWebsiteDomainName,
+                        originProtocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+                    },
+                    behaviors: [{isDefaultBehavior: true}],
+                }
+            ]
+        });
+        new cdk.CfnOutput(this, 'DistributionId', {value: distribution.distributionId});
+
+        // Route53 alias record for the CloudFront distribution
+        new route53.ARecord(this, 'SiteAliasRecord', {
+            recordName: siteDomain,
+            target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+            zone
+        });
+
+        // Deploy site contents to S3 bucket
+        new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
+            sources: [s3deploy.Source.asset('../dist')],
+            destinationBucket: siteBucket,
+            distribution,
+            distributionPaths: ['/*'],
+        });
+    }
+}

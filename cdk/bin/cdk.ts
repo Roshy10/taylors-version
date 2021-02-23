f1298be4b8a53917fc5766cb552ada35ebfb1d6f
Roshan Bhumbra <rbhumbra@gmail.com>
2021-02-23 11:58:31 +0000

    change env to use default account

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -30,7 +30,8 @@ const stack = new MyStaticSiteStack(app, 'TaylorsVersion', {
     env: {
         // Stack must be in us-east-1, because the ACM certificate for a
         // global CloudFront distribution must be requested in us-east-1.
-        region: 'us-east-1'
+        region: 'us-east-1',
+        account: "default"
     }
 });
 

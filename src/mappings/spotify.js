a61686581e02b3c94d42cce388510745ea9f5e03
Roshan Bhumbra <rbhumbra@gmail.com>
2021-04-09 16:41:29 +0100

    modified the way deleting replaced tracks is done to work around spotify api quirks added some sneaky extra albums that exist but aren't listed

--- src/mappings/spotify.js
+++ src/mappings/spotify.js
@@ -1,155 +1,341 @@
-const spotify = {
+const spotify = [
 
     // Album - Fearless
     // Fearless
-    // original
-    "spotify:track:6Eu31gddWw0gOGO506pJYA": "spotify:track:77sMIMlNaSURUAXq5coCxE",
-    "spotify:track:61v7awvaSeBXJHBKMjw59I": "spotify:track:77sMIMlNaSURUAXq5coCxE",
-    // BMRRS
-    "spotify:track:15PRxID4aGtOncbffzgZXI": "spotify:track:77sMIMlNaSURUAXq5coCxE",
-    // PE
-    "spotify:track:2okho7vU7Nsq1UZD0kgIMi": "spotify:track:77sMIMlNaSURUAXq5coCxE",
-    // IV
-    "spotify:track:6zV8AVM9eVfdG7Qahwd64R": "spotify:track:77sMIMlNaSURUAXq5coCxE",
+    {
+        "replacement": "spotify:track:77sMIMlNaSURUAXq5coCxE",
+        "originals": [
+            // original
+            "spotify:track:6Eu31gddWw0gOGO506pJYA",
+            // original extended
+            "spotify:track:61v7awvaSeBXJHBKMjw59I",
+            // BMRRS
+            "spotify:track:15PRxID4aGtOncbffzgZXI",
+            // PE
+            "spotify:track:2okho7vU7Nsq1UZD0kgIMi",
+            // PE unavailable
+            "spotify:track:4adub9Gizi8N2zCjq7XSpD",
+            // IV
+            "spotify:track:6zV8AVM9eVfdG7Qahwd64R",
+        ],
+    },
 
     // Fifteen
-    // BMRRS
-    "spotify:track:6aQrB8kaWyaCj9RYB7Rh5L": "spotify:track:2nqio0SfWg6gh2eCtfuMa5",
-    // PE
-    "spotify:track:5Ora3Ish2A1JN5veGYTxhw": "spotify:track:2nqio0SfWg6gh2eCtfuMa5",
-    // IV
-    "spotify:track:4yf9OivCH2lmQWN8mzpCC6": "spotify:track:2nqio0SfWg6gh2eCtfuMa5",
-    // rep STSSP
-    "spotify:track:0H2UD0LfSCiRr59YMGlCuS": "spotify:track:550erGcdD9n6PnwxrvYqZT",
+    {
+        "replacement": "spotify:track:2nqio0SfWg6gh2eCtfuMa5",
+        "originals": [
+            // original
+            "spotify:track:4t0OI7XrODjSkAu3bTPmWj",
+            // original extended
+            "spotify:track:07gxAwYjBbZdhmkxpMM0GP",
+            // BMRRS
+            "spotify:track:6aQrB8kaWyaCj9RYB7Rh5L",
+            // PE
+            "spotify:track:5Ora3Ish2A1JN5veGYTxhw",
+            // PE unavailable
+            "spotify:track:540uR4mP2DbmzOO4ydXsRX",
+            // IV
+            "spotify:track:4yf9OivCH2lmQWN8mzpCC6",
+            // rep STSSP
+            "spotify:track:0H2UD0LfSCiRr59YMGlCuS",
+        ],
+    },
 
     // Love Story
-    // BMRRS
-    "spotify:track:1D4PL9B8gOg78jiHg3FvBb": "spotify:track:3CeCwYWvdfXbZLXFhBrbnf",
-    // PE
-    "spotify:track:5hnJCRSo4pxRsaEwT9YusG": "spotify:track:3CeCwYWvdfXbZLXFhBrbnf",
-    // IV
-    "spotify:track:5xPsybHWRHbTYO44xnREY5": "spotify:track:3CeCwYWvdfXbZLXFhBrbnf",
+    {
+        "replacement": "spotify:track:3CeCwYWvdfXbZLXFhBrbnf",
+        "originals": [
+            // original
+            "spotify:track:1vrd6UOGamcKNGnSHJQlSt",
+            // original extended
+            "spotify:track:6K1sUckiJLXhsyte2yNFTQ",
+            // BMRRS
+            "spotify:track:1D4PL9B8gOg78jiHg3FvBb",
+            // PE
+            "spotify:track:5hnJCRSo4pxRsaEwT9YusG",
+            // PE unavailable
+            "spotify:track:4ikR3lDFcZ0hNKyVONfi4N",
+            // IV
+            "spotify:track:5xPsybHWRHbTYO44xnREY5",
+        ],
+    },
 
     // Hey Stephen
-    // BMRRS
-    "spotify:track:3DTqKBlJPlRSclFfBI0ZoE": "spotify:track:550erGcdD9n6PnwxrvYqZT",
-    // PE
-    "spotify:track:3CI1f4yLwdHzcUnH7o0Xyh": "spotify:track:550erGcdD9n6PnwxrvYqZT",
-    // IV
-    "spotify:track:4fWDgRZa0GKE5KZNhE9MQa": "spotify:track:550erGcdD9n6PnwxrvYqZT",
-    // rep STSSP
-    "spotify:track:0TYJbIHBtmWy9ugtU9BcbE": "spotify:track:550erGcdD9n6PnwxrvYqZT",
+    {
+        "replacement": "spotify:track:550erGcdD9n6PnwxrvYqZT",
+        "originals": [
+            // original
+            "spotify:track:4WXzzCof26KJLTK5kK53dS",
+            // original extended
+            "spotify:track:5SyE6zNX1wSRobzxNkeX43",
+            // BMRRS
+            "spotify:track:3DTqKBlJPlRSclFfBI0ZoE",
+            // PE
+            "spotify:track:3CI1f4yLwdHzcUnH7o0Xyh",
+            // PE unavailable
+            "spotify:track:32pM2R8GQLtbBKdSwdTAM1",
+            // IV
+            "spotify:track:4fWDgRZa0GKE5KZNhE9MQa",
+            // rep STSSP
+            "spotify:track:0TYJbIHBtmWy9ugtU9BcbE",
+        ],
+    },
 
     // White Horse
-    // BMRRS
-    "spotify:track:4QhjEJ2o6QNwpZGs51AmiY": "spotify:track:5YL553x8sHderRBDlm3NM3",
-    // PE
-    "spotify:track:5yKB8qcTSflDercM2PXwot": "spotify:track:5YL553x8sHderRBDlm3NM3",
-    // IV
-    "spotify:track:1yFjBRQVhRdcdnrTE5LuyD": "spotify:track:5YL553x8sHderRBDlm3NM3",
-    // rep STSSP
-    "spotify:track:4RMfSYDFkcz0bKoK8fHDgR": "spotify:track:550erGcdD9n6PnwxrvYqZT",
+    {
+        "replacement": "spotify:track:5YL553x8sHderRBDlm3NM3",
+        "originals": [
+            // original
+            "spotify:track:6wn61Fzx9XMxQmieLpoIhW",
+            // original extended
+            "spotify:track:2OGA4hbUqiAhCQj1pxeNld",
+            // BMRRS
+            "spotify:track:4QhjEJ2o6QNwpZGs51AmiY",
+            // PE
+            "spotify:track:5yKB8qcTSflDercM2PXwot",
+            // PE unavailable
+            "spotify:track:1vicNVzWoAnteQMlTzI2VF",
+            // IV
+            "spotify:track:1yFjBRQVhRdcdnrTE5LuyD",
+            // rep STSSP
+            "spotify:track:4RMfSYDFkcz0bKoK8fHDgR",
+        ],
+    },
 
     // You Belong With Me
-    // BMRRS
-    "spotify:track:1GEBsLDvJGw7kviySRI6GX": "spotify:track:1qrpoAMXodY6895hGKoUpA",
-    // PE
-    "spotify:track:4kgTdThcDHfuDS2kKxB7Lc": "spotify:track:1qrpoAMXodY6895hGKoUpA",
-    // IV
-    "spotify:track:7G6TDmGJXDoBaV6NGj3Yic": "spotify:track:1qrpoAMXodY6895hGKoUpA",
+    {
+        "replacement": "spotify:track:1qrpoAMXodY6895hGKoUpA",
+        "originals": [
+            // original
+            "spotify:track:3GCL1PydwsLodcpv0Ll1ch",
+            // original extended
+            "spotify:track:6mqdunuFFSODHKcpDTFvAj",
+            // BMRRS
+            "spotify:track:1GEBsLDvJGw7kviySRI6GX",
+            // PE
+            "spotify:track:4kgTdThcDHfuDS2kKxB7Lc",
+            // PE unavailable
+            "spotify:track:26eccs3bbw6DMekFwZbdL2",
+            // IV
+            "spotify:track:7G6TDmGJXDoBaV6NGj3Yic",
+        ],
+    },
 
     // Breathe
-    // BMRRS
-    "spotify:track:2dzk2z7MXl0peRhBUsDq4d": "spotify:track:7HC7R2D8WjXVcUHJyEGjRs",
-    // PE
-    "spotify:track:23qIWGhpwvDgC23TyZOUck": "spotify:track:7HC7R2D8WjXVcUHJyEGjRs",
-    // IV
-    "spotify:track:0qsSmObOgBZfuMIwfAQDBP": "spotify:track:7HC7R2D8WjXVcUHJyEGjRs",
-    // rep STSSP
-    "spotify:track:3oxgFdIlxFEDEJqn7k5TVN": "spotify:track:550erGcdD9n6PnwxrvYqZT",
+    {
+        "replacement": "spotify:track:7HC7R2D8WjXVcUHJyEGjRs",
+        "originals": [
+            // original
+            "spotify:track:49mWEy5MgtNujgT7xU3emT",
+            // original extended
+            "spotify:track:3uvCE2wdHDyP0AojlmPlUa",
+            // BMRRS
+            "spotify:track:2dzk2z7MXl0peRhBUsDq4d",
+            // PE
+            "spotify:track:23qIWGhpwvDgC23TyZOUck",
+            // PE unavailable
+            "spotify:track:20h5sJ9pElnXT4cbVUTkHs",
+            // IV
+            "spotify:track:0qsSmObOgBZfuMIwfAQDBP",
+            // rep STSSP
+            "spotify:track:3oxgFdIlxFEDEJqn7k5TVN",
+        ],
+    },
 
     // Tell Me Why
-    // BMRRS
-    "spotify:track:7bUvIh9rQsboJBXaSIQZAF": "spotify:track:0k0vFacOHNuArLWMiH60p7",
-    // PE
-    "spotify:track:4x7Gxk2GfwZJcqpoVT1PbR": "spotify:track:0k0vFacOHNuArLWMiH60p7",
-    // IV
-    "spotify:track:6vLpy7UPYuOa75Bkqocrcw": "spotify:track:0k0vFacOHNuArLWMiH60p7",
+    {
+        "replacement": "spotify:track:0k0vFacOHNuArLWMiH60p7",
+        "originals": [
+            // original
+            "spotify:track:3rnI1UCyGJvUTVvT97VQr5",
+            // original extended
+            "spotify:track:5du2fre21jWENkp1IDRaXf",
+            // BMRRS
+            "spotify:track:7bUvIh9rQsboJBXaSIQZAF",
+            // PE
+            "spotify:track:4x7Gxk2GfwZJcqpoVT1PbR",
+            // PE unavailable
+            "spotify:track:6DfeQ1pT4ClDJKLeQOTnKW",
+            // IV
+            "spotify:track:6vLpy7UPYuOa75Bkqocrcw",
+        ],
+    },
 
     // You're Not Sorry
-    // BMRRS
-    "spotify:track:0ReNbPVMU7jUWqn1yHo9cE": "spotify:track:6iiAfo4wTA2CVC3Uwx9uh8",
-    // PE
-    "spotify:track:3JhvYnOfKLJuWC7jEU0GmJ": "spotify:track:6iiAfo4wTA2CVC3Uwx9uh8",
-    // IV
-    "spotify:track:4N419mmxYybTfAECy5j4dw": "spotify:track:6iiAfo4wTA2CVC3Uwx9uh8",
+    {
+        "replacement": "spotify:track:6iiAfo4wTA2CVC3Uwx9uh8",
+        "originals": [
+            // original
+            "spotify:track:0HmCuN0Z3OX1Qrz43FLOPL",
+            // original extended
+            "spotify:track:5SSYD3BoD18792DOlW0h5V",
+            // BMRRS
+            "spotify:track:0ReNbPVMU7jUWqn1yHo9cE",
+            // PE
+            "spotify:track:3JhvYnOfKLJuWC7jEU0GmJ",
+            // PE unavailable
+            "spotify:track:7zen3j1316Tsk5iEZuDOft",
+            // IV
+            "spotify:track:4N419mmxYybTfAECy5j4dw",
+        ],
+    },
 
     // The Way I Loved You
-    // BMRRS
-    "spotify:track:54ziHHVIBJg3YJ9sn17mEy": "spotify:track:22bPsP2jCgbLUvh82U0Z3M",
-    // PE
-    "spotify:track:4pEZUytwBzha5Un6m3SCFf": "spotify:track:22bPsP2jCgbLUvh82U0Z3M",
-    // IV
-    "spotify:track:04LDcz3RX1P5sIlzQGqgUN": "spotify:track:22bPsP2jCgbLUvh82U0Z3M",
+    {
+        "replacement": "spotify:track:22bPsP2jCgbLUvh82U0Z3M",
+        "originals": [
+            // original
+            "spotify:track:5P4wWhUYWM0IaVYLuZxdar",
+            // original extended
+            "spotify:track:6MNPOhGO5dv15nqei7GN0D",
+            // BMRRS
+            "spotify:track:54ziHHVIBJg3YJ9sn17mEy",
+            // PE
+            "spotify:track:4pEZUytwBzha5Un6m3SCFf",
+            // PE unavailable
+            "spotify:track:7h8gWtGOVLLpgOvVYNFqC7",
+            // IV
+            "spotify:track:04LDcz3RX1P5sIlzQGqgUN",
+        ],
+    },
 
     // Forever & Always
-    // BMRRS
-    "spotify:track:5VHdgl9hFUmabiCO5jeISv": "spotify:track:1msEuwSBneBKpVCZQcFTsU",
-    // PE
-    "spotify:track:3NKCWBT2uKp8rxSkcvIrdX": "spotify:track:1msEuwSBneBKpVCZQcFTsU",
-    // IV
-    "spotify:track:3yuOL0jFnVR8tizZzNNo0F": "spotify:track:1msEuwSBneBKpVCZQcFTsU",
-    // rep STSSP
-    "spotify:track:0QCS4NpURHvWdF1icdkfBw": "spotify:track:550erGcdD9n6PnwxrvYqZT",
+    {
+        "replacement": "spotify:track:1msEuwSBneBKpVCZQcFTsU",
+        "originals": [
+            // original
+            "spotify:track:47HtKpfzpAt8rQjjXWotFj",
+            // original extended
+            "spotify:track:3wxkYiniEnp0ZafDE5vD07",
+            // BMRRS
+            "spotify:track:5VHdgl9hFUmabiCO5jeISv",
+            // PE
+            "spotify:track:3NKCWBT2uKp8rxSkcvIrdX",
+            // PE unavailable
+            "spotify:track:4oYMFlaSqdoID82aQtXPYg",
+            // IV
+            "spotify:track:3yuOL0jFnVR8tizZzNNo0F",
+            // rep STSSP
+            "spotify:track:0QCS4NpURHvWdF1icdkfBw",
+        ],
+    },
 
     // The Best Day
-    // BMRRS
-    "spotify:track:0ck1qErO76fM9xKxuSeqDk": "spotify:track:6ON9UuIq49xXY9GPmHIYRp",
-    // PE
-    "spotify:track:7CjuAoOiSrwWv7wJonAUmw": "spotify:track:6ON9UuIq49xXY9GPmHIYRp",
-    // IV
-    "spotify:track:7G9D32Kx7hdQ9Uga1V4VXm": "spotify:track:6ON9UuIq49xXY9GPmHIYRp",
+    {
+        "replacement": "spotify:track:6ON9UuIq49xXY9GPmHIYRp",
+        "originals": [
+            // original
+            "spotify:track:3esA216TyLHEkNiBCeCmcg",
+            // original extended
+            "spotify:track:4G5YAPiKudJCDmICzj0Zwk",
+            // BMRRS
+            "spotify:track:0ck1qErO76fM9xKxuSeqDk",
+            // PE
+            "spotify:track:7CjuAoOiSrwWv7wJonAUmw",
+            // PE unavailable
+            "spotify:track:0U04szAxhhjbqHgwy9ndoh",
+            // IV
+            "spotify:track:7G9D32Kx7hdQ9Uga1V4VXm",
+        ],
+    },
 
     // Change
-    // BMRRS
-    "spotify:track:6nIUWFgMly8cdh32SbnhiR": "spotify:track:3ExweHKZF9B752DPQByRVT",
-    // PE
-    "spotify:track:3gFaNwyiafgUoQhR0pHYA9": "spotify:track:3ExweHKZF9B752DPQByRVT",
-    // IV
-    "spotify:track:493jXNrYmXPCMbSJvVRYLk": "spotify:track:3ExweHKZF9B752DPQByRVT",
-    // rep STSSP
-    "spotify:track:5PSOWHll3h3Vy226xSJk0M": "spotify:track:550erGcdD9n6PnwxrvYqZT",
+    {
+        "replacement": "spotify:track:3ExweHKZF9B752DPQByRVT",
+        "originals": [
+            // original
+            "spotify:track:1yACRKAwlXWhXXFUSkvzhD",
+            // original extended
+            "spotify:track:6rEdUI8O9sPKbN1HQKnbrL",
+            // BMRRS
+            "spotify:track:6nIUWFgMly8cdh32SbnhiR",
+            // PE
+            "spotify:track:3gFaNwyiafgUoQhR0pHYA9",
+            // PE unavailable
+            "spotify:track:4EnxFZptr8J9Ytzz7uyElf",
+            // IV
+            "spotify:track:493jXNrYmXPCMbSJvVRYLk",
+            // rep STSSP
+            "spotify:track:5PSOWHll3h3Vy226xSJk0M",
+        ],
+    },
 
     // Jump Then Fall
-    // PE
-    "spotify:track:6WWvcA62QlnRguluWAk6bv": "spotify:track:2m3ObD945KvpE5y9A1eUWm",
-    // rep STSSP
-    "spotify:track:5zytSTR2g0I9psX2Z12ex6": "spotify:track:550erGcdD9n6PnwxrvYqZT",
+    {
+        "replacement": "spotify:track:2m3ObD945KvpE5y9A1eUWm",
+        "originals": [
+            // PE
+            "spotify:track:6WWvcA62QlnRguluWAk6bv",
+            // PE unavailable
+            "spotify:track:4mvQXr3EeckZIBmlcxLqXe",
+            // rep STSSP
+            "spotify:track:5zytSTR2g0I9psX2Z12ex6",
+        ],
+    },
 
     // Untouchable
-    // PE
-    "spotify:track:6ovFJkdau79DuXEvQjPaNH": "spotify:track:0tQ9vBYpldCuikPsbgOVKA",
+    {
+        "replacement": "spotify:track:0tQ9vBYpldCuikPsbgOVKA",
+        "originals": [
+            // PE
+            "spotify:track:6ovFJkdau79DuXEvQjPaNH",
+            // PE unavailable
+            "spotify:track:5IvkGi34lJuiZp9gdlafJ1",
+        ],
+    },
 
     // Forever & Always (Piano Version)
-    // PE
-    "spotify:track:13MAWVUSkbGMUi6I7RowRK": "spotify:track:01QdEx6kFr78ZejhQtWR5m",
+    {
+        "replacement": "spotify:track:01QdEx6kFr78ZejhQtWR5m",
+        "originals": [
+            // PE
+            "spotify:track:13MAWVUSkbGMUi6I7RowRK",
+            // PE unavailable
+            "spotify:track:1QK0cJlXnfq3q7Vo8mVL4w",
+        ],
+    },
 
     // Come In With The Rain
-    // PE
-    "spotify:track:0cEG04R5cldkTLzK6phlcn": "spotify:track:1n2wszmJyVkw6FHqyLnQsY",
+    {
+        "replacement": "spotify:track:1n2wszmJyVkw6FHqyLnQsY",
+        "originals": [
+            // PE
+            "spotify:track:0cEG04R5cldkTLzK6phlcn",
+            // PE unavailable
+            "spotify:track:4xLiykjcfrGMkzsHHK4Lhh",
+        ],
+    },
 
     // SuperStar
-    // PE
-    "spotify:track:0XfTh16YcKPzdxMPTeKTAq": "spotify:track:51A8eKvvZz9uydvIZ7xRSV",
+    {
+        "replacement": "spotify:track:51A8eKvvZz9uydvIZ7xRSV",
+        "originals": [
+            // PE
+            "spotify:track:0XfTh16YcKPzdxMPTeKTAq",
+            // PE unavailable
+            "spotify:track:313c46YL18vX1rKsyF5oc6",
+        ],
+    },
 
     // The Other Side Of The Door
-    // PE
-    "spotify:track:1yDJcXc549N6AabNBzFAku": "spotify:track:1cSFlSBdpT4F5vb1frQ231",
+    {
+        "replacement": "spotify:track:1cSFlSBdpT4F5vb1frQ231",
+        "originals": [
+            // PE
+            "spotify:track:1yDJcXc549N6AabNBzFAku",
+            // PE unavailable
+            "spotify:track:7JbxkpFLB6pvuNu3buoxAj",
+        ],
+    },
 
     // Today Was A Fairytale
-    // Single
-    "spotify:track:4pFvEWbjBpPUdYRQly0THs": "spotify:track:2JoJrsEV15OzbijS47lids",
-
-};
+    {
+        "replacement": "spotify:track:2JoJrsEV15OzbijS47lids",
+        "originals": [
+            // Single
+            "spotify:track:4pFvEWbjBpPUdYRQly0THs",
+        ],
+    },
+];
 
 export default spotify;

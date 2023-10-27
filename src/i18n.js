7a2fb1460ce9010d563bf307f67c3e6711cc323e
melvinswift <greatwinblowaway@gmail.com>
2023-10-27 20:35:13 +0700

    add th

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,13 @@
 import i18n from "i18next";
+import detector from "i18next-browser-languagedetector";
 import {initReactI18next} from "react-i18next";
 import en from "./locales/en.json";
+import th from "./locales/th.json";
 
-const resources = {en};
+const resources = {en, th};
 
 i18n
+    .use(detector)
     // pass the locales instance to react-i18next.
     .use(initReactI18next)
     // init i18next
@@ -12,6 +15,7 @@ i18n
     .init({
         debug: false,
         defaultNS: "taylorsVersion",
+        supportedLngs: ["en", "th"],
         fallbackLng: "en",
         resources,
         detection: {

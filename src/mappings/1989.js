3b9401db2d920bbc13a5d48ff511ca61c42ee0f5
Roshan Bhumbra <Roshy10@users.noreply.github.com>
2023-10-27 09:14:05 +0100

    add 1989 tracks

---
+++ src/mappings/1989.js
@@ -0,0 +1,160 @@
+const album_1989 = [
+    {   // Welcome To New York
+        "replacement": "spotify:track:1hR8BSuEqPCCZfv93zzzz9",
+        "originals": [
+            "spotify:track:3nRmDz7qGCvsMS30rGGY0x",
+            "spotify:track:1YYOx55cW5JfMe65zEHBdu",
+            "spotify:track:10nqz67NQWWa7XPq7ycihi",
+            "spotify:track:3YLA3NgzFRRa7LiHMtFPby",
+            "spotify:track:7aMftpNNJiQj1c2rllc8Dk",
+        ],
+    },
+    {   // Blank Space
+        "replacement": "https://open.spotify.com/track/45wMBGri1PORPjM9PwFfrS?si=52a2f5d7d9cd4aef",
+        "originals": [
+            "spotify:track:0jjIPthFhpbBNr4my8cePO",
+            "spotify:track:1u8c2t2Cy7UBoG4ArRcF5g",
+            "spotify:track:3LB2qckfpZq7aybaoZKRa2",
+            "spotify:track:2ls70nUDfjzm1lSRDuKxmw",
+            "spotify:track:1kHEuJRasudLhjvnbfc4yS",
+        ],
+    },
+    {   // Style
+        "replacement": "https://open.spotify.com/track/1hjRhYpWyqDpPahmSlUTlc?si=69134efbfcf948c2",
+        "originals": [
+            "spotify:track:60636BsHWxFi7SfYxbP0UV",
+            "spotify:track:0ug5NqcwcFR2xrfTkc7k8e",
+            "spotify:track:71PmZqBXH0RUETqxpwlV0w",
+            "spotify:track:4D7kxLhSz7siCOw7KA5rQn",
+        ],
+    },
+    {   // Out Of The Woods
+        "replacement": "https://open.spotify.com/track/045ZeOHPIzhxxsm8bq5kyE?si=49e6f770cde94fb6",
+        "originals": [
+            "spotify:track:6YHzSWA80uf3LHYMGoDZlM",
+            "spotify:track:0qUnBLZ8bJqUNEeQgayL9t",
+            "spotify:track:7CkJgmJ3RFU6jNXNHgRkBw",
+            "spotify:track:0GmfiNHSgkKamYgQEMYOH3",
+            "spotify:track:2zbVATcYiggJSWeSZkDFgd",
+            "spotify:track:7EpVQjqtKygiwJIASxMWNg",
+        ],
+    },
+    {   // All You Had To Do Was Stay
+        "replacement": "https://open.spotify.com/track/6GXgd1BPD9bUpqw5AntGV5?si=7f0978d59cad41cd",
+        "originals": [
+            "spotify:track:50Pu9HKAcPm8TFL2zDrouh",
+            "spotify:track:6aLOekfwbytwWvQftxTEF0",
+            "spotify:track:4NNolUeL5m2ju7kf8pdg6H",
+        ],
+    },
+    {   // Shake It Off
+        "replacement": "https://open.spotify.com/track/3pv7Q5v2dpdefwdWIvE7yH?si=369c1e8db0cb4645",
+        "originals": [
+            "spotify:track:4mEk3TJYIxh1ISDzEpkYoF",
+            "spotify:track:4ZspJzYIxOztSc9hZmcLzH",
+            "spotify:track:5oID3Qj3tTCZEH9eo0snxm",
+            "spotify:track:0cqRj7pUJDkTCEsJkx8snD",
+            "spotify:track:3OtAiLvFzNB8KMnzdyHVnq",
+            "spotify:track:6PRELLjOe8RaRIcW0sHskx",
+            "spotify:track:7BNBo9g7x0EK46d44HFkKH",
+            "spotify:track:2JH2oziHRcxEFUHgfZNm2S",
+            "spotify:track:5pJvzBpE1KSMxEgMg5Ft31",
+            "spotify:track:1CiVz2WkmZRwhU3A6J2Gjz",
+            "spotify:track:0PEZYlPV0BxoLl9M3pjVnP",
+            "spotify:track:4RPkqiTSRzdo0RPg13bE8n",
+            "spotify:track:6zpQwkOGOTt6OCnMdNKjHl",
+            "spotify:track:1kKlhuQE0HXp1IwBRpaH2P",
+            "spotify:track:21txIeEaimkRCzPOH8L9HG",
+        ],
+    },
+    {   // I Wish You Would
+        "replacement": "https://open.spotify.com/track/43y1WpBdnEy5TR9aZoSQL9?si=669dcf2e28f94bd9",
+        "originals": [
+            "spotify:track:5gRYrtvyVyaCRvLt56OfuV",
+            "spotify:track:3JPOspEefgmGT5Hyy11oQJ",
+            "spotify:track:5RVJ6VKfwtosA5Q1qeHdjF",
+            "spotify:track:0t0MOVBcdiOXS8FblHewBx",
+        ],
+    },
+    {   // Bad Blood
+        "replacement": "https://open.spotify.com/track/64FzgoLZ3oXu2SriZblHic?si=9af0e5cd3b314f57",
+        "originals": [
+            "spotify:track:2NlmmAjGYrrjAp0MED5rGx",
+            "spotify:track:4SHyS2ThjrS1iBGruKweCz",
+            "spotify:track:0TvQLMecTE8utzoNmvXRbK",
+            "spotify:track:5QmXcfqaz8TobfNbkoDHvP",
+            "spotify:track:6xsEAm6w9oMQYYg3jkEkMT",
+        ],
+    },
+    {   // Wildest Dreams
+        "replacement": "https://open.spotify.com/track/1K39ty6o1sHwwlZwO6a7wK?si=9dd9e9cac6714509",
+        "originals": [
+            "spotify:track:3fVnlF4pGqWI9flVENcT28",
+            "spotify:track:3ScVaMDQwaD8iumBYzcDvr",
+            "spotify:track:22C0JIVhFaczZ4t9heqREN",
+            "spotify:track:4A4A6dwl5DqdqrZaEtKGPR",
+            "spotify:track:59HjlYCeBsxdI0fcm3zglw",
+            "spotify:track:106R7Z57WYzBAfrXImV30y",
+        ],
+    },
+    {   // How You Get The Girl
+        "replacement": "https://open.spotify.com/track/75W3SngKzTuoQ94uLf3y82?si=a979ced11bd64204",
+        "originals": [
+            "spotify:track:0fM9dEhUFV4MHDuJgrcfOv",
+            "spotify:track:1l6KjnsqP2atawKgBV49u3",
+            "spotify:track:5Z9ocF6ng0m2haHnKtHwTp",
+            "spotify:track:3aSYZ4upQ6OSNos26YeVWL",
+        ],
+    },
+    {   // This Love
+        "replacement": "https://open.spotify.com/track/5QVVjX0ZItqlVpEuVCM9Yg?si=077abc2641b24673",
+        "originals": [
+            "spotify:track:7lTxQZVXlvIX7Z3szSy8Sh",
+            "spotify:track:64KRxmbqypPBlya4nxFmDn",
+            "spotify:track:5yCRSEPJXzjzl3Gov0YduA",
+            "spotify:track:4L560fic5PaTuCFm6CviKa",
+            "spotify:track:3oKMl2tJv4fdidkXUYMI5x",
+            "spotify:track:1kTPQnabROVkW9bUXdCGrB",
+            "spotify:track:28VqgFAhYCU7FdAm7dvEjp",
+        ],
+    },
+    {   // I Know Places
+        "replacement": "https://open.spotify.com/track/2f0GI2ZLUtbGqFx8t2Gk6A?si=b4903ae32c414055",
+        "originals": [
+            "spotify:track:6Ck9WerfsyvC4v4AylUoDp",
+            "spotify:track:3qGLcmO0XnnrcpAQGZwTvA",
+            "spotify:track:6EwNJz8CuVsrsLvXprJ20Q",
+            "spotify:track:1fDfzSgIEFWDuX383vRiAv",
+        ],
+    },
+    {   // Clean
+        "replacement": "https://open.spotify.com/track/2ByBBvpR9b7IynvRmnG7kG?si=2296d8aa0bcd4166",
+        "originals": [
+            "spotify:track:1s4CiRufJUyeyQT6HRmu7S",
+            "spotify:track:2k43eySbfpDVx0PrO99UI8",
+            "spotify:track:1NmVZsG18CzCAtw7rnV3yA",
+        ],
+    },
+    {   // Wonderland
+        "replacement": "https://open.spotify.com/track/2ve0kuaWUCt4Zx8x4bf1MR?si=1681f2786b464203",
+        "originals": [
+            "spotify:track:6RvRzl1YJTDnUvdOtV21IK",
+            "spotify:track:0eTCaVOYLpJlSkbEi2jay4",
+        ],
+    },
+    {   // You Are In Love
+        "replacement": "https://open.spotify.com/track/4UwqOWDpdeIDVQDuKT6iza?si=0c41b4034b884603",
+        "originals": [
+            "spotify:track:5CMw15fx1o2OW2jzaYy36L",
+            "spotify:track:2KrOAg6FftbjgSKdd2a4rS",
+        ],
+    },
+    {   // New Romantics
+        "replacement": "https://open.spotify.com/track/2vPMoMDXxu9uX1igWZmXSG?si=a0183e61a2df47f5",
+        "originals": [
+            "spotify:track:0qAIiGFKLdV1xpNlEhjpq8",
+        ],
+    },
+];
+
+export default album_1989;

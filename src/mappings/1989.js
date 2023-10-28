d4badb1d9b6dab9e07d283d2be1ea5770e7458b8
melvinswift <greatwinblowaway@gmail.com>
2023-10-28 14:16:12 +0700

    update with deluxe

--- src/mappings/1989.js
+++ src/mappings/1989.js
@@ -1,7 +1,7 @@
 const album_1989 = [
     // Welcome to New York
     {
-        "replacement": "spotify:track:1hR8BSuEqPCCZfv93zzzz9",
+        "replacement": "spotify:track:4WUepByoeqcedHoYhSNHRt",
         "originals": [
             // original
             "spotify:track:10nqz67NQWWa7XPq7ycihi",
@@ -15,11 +15,13 @@ const album_1989 = [
             "spotify:track:1YYOx55cW5JfMe65zEHBdu",
             // reputation Stadium Tour Surprise Song Playlist
             "spotify:track:3YLA3NgzFRRa7LiHMtFPby",
+            // 1989 TV standard
+            "spotify:track:1hR8BSuEqPCCZfv93zzzz9"
         ],
     },
     // Blank Space
     {
-        "replacement": "spotify:track:45wMBGri1PORPjM9PwFfrS",
+        "replacement": "spotify:track:0108kcWLnn2HlH2kedi1gn",
         "originals": [
             // original
             "spotify:track:2ls70nUDfjzm1lSRDuKxmw",
@@ -29,13 +31,15 @@ const album_1989 = [
             "spotify:track:3LB2qckfpZq7aybaoZKRa2",
             // BMRRS
             "spotify:track:1kHEuJRasudLhjvnbfc4yS",
-            // BM //1's Vol.4
+            // BM #1's Vol.4
             "spotify:track:0jjIPthFhpbBNr4my8cePO",
+            // 1989 TV standard
+            "spotify:track:45wMBGri1PORPjM9PwFfrS"
         ],
     },
     // Style
     {
-        "replacement": "spotify:track:1hjRhYpWyqDpPahmSlUTlc",
+        "replacement": "spotify:track:3Vpk1hfMAQme8VJ0SNRSkd",
         "originals": [
             // original
             "spotify:track:71PmZqBXH0RUETqxpwlV0w",
@@ -43,13 +47,15 @@ const album_1989 = [
             "spotify:track:0ug5NqcwcFR2xrfTkc7k8e",
             // BMRRS
             "spotify:track:4D7kxLhSz7siCOw7KA5rQn",
-            // BM //1's Vol.4
+            // BM #1's Vol.4
             "spotify:track:60636BsHWxFi7SfYxbP0UV",
+            // 1989 TV standard
+            "spotify:track:1hjRhYpWyqDpPahmSlUTlc"
         ],
     },
     // Out of the Woods
     {
-        "replacement": "spotify:track:045ZeOHPIzhxxsm8bq5kyE",
+        "replacement": "spotify:track:1OcSfkeCg9hRC2sFKB4IMJ",
         "originals": [
             // original
             "spotify:track:7EpVQjqtKygiwJIASxMWNg",
@@ -61,11 +67,13 @@ const album_1989 = [
             "spotify:track:6YHzSWA80uf3LHYMGoDZlM",
             "spotify:track:0GmfiNHSgkKamYgQEMYOH3",
             "spotify:track:2zbVATcYiggJSWeSZkDFgd",
+            // 1989 TV standard
+            "spotify:track:045ZeOHPIzhxxsm8bq5kyE"
         ],
     },
     // All You Had To Do Was Stay
     {
-        "replacement": "spotify:track:6GXgd1BPD9bUpqw5AntGV5",
+        "replacement": "spotify:track:2k0ZEeAqzvYMcx9Qt5aClQ",
         "originals": [
             // original
             "spotify:track:4NNolUeL5m2ju7kf8pdg6H",
@@ -73,11 +81,13 @@ const album_1989 = [
             "spotify:track:6aLOekfwbytwWvQftxTEF0",
             // BMRRS
             "spotify:track:50Pu9HKAcPm8TFL2zDrouh",
+            // 1989 TV standard
+            "spotify:track:6GXgd1BPD9bUpqw5AntGV5"
         ],
     },
     // Shake It Off
     {
-        "replacement": "spotify:track:3pv7Q5v2dpdefwdWIvE7yH",
+        "replacement": "spotify:track:50yNTF0Od55qnHLxYsA5Pw",
         "originals": [
             // original
             "spotify:track:1kKlhuQE0HXp1IwBRpaH2P",
@@ -102,11 +112,13 @@ const album_1989 = [
             "spotify:track:4RPkqiTSRzdo0RPg13bE8n",
             "spotify:track:6zpQwkOGOTt6OCnMdNKjHl",
             "spotify:track:21txIeEaimkRCzPOH8L9HG",
+            // 1989 TV standard
+            "spotify:track:3pv7Q5v2dpdefwdWIvE7yH"
         ],
     },
     // I Wish You Would
     {
-        "replacement": "spotify:track:43y1WpBdnEy5TR9aZoSQL9",
+        "replacement": "spotify:track:3FxJDucHWdw6caWTKO5b23",
         "originals": [
             // original
             "spotify:track:5RVJ6VKfwtosA5Q1qeHdjF",
@@ -116,11 +128,13 @@ const album_1989 = [
             "spotify:track:3JPOspEefgmGT5Hyy11oQJ",
             // Country Workout Vol.1
             "spotify:track:0t0MOVBcdiOXS8FblHewBx",
+            // 1989 TV standard
+            "spotify:track:43y1WpBdnEy5TR9aZoSQL9"
         ],
     },
     // Bad Blood
     {
-        "replacement": "spotify:track:64FzgoLZ3oXu2SriZblHic",
+        "replacement": "spotify:track:7oZONwFiFIErZcXAtTu7FY",
         "originals": [
             // original
             "spotify:track:0TvQLMecTE8utzoNmvXRbK",
@@ -132,11 +146,13 @@ const album_1989 = [
             "spotify:track:6xsEAm6w9oMQYYg3jkEkMT",
             // Be my Anti-Valentine 2022
             "spotify:track:5QmXcfqaz8TobfNbkoDHvP",
+            // 1989 TV standard
+            "spotify:track:64FzgoLZ3oXu2SriZblHic"
         ],
     },
     // Wildest Dreams
     {
-        "replacement": "spotify:track:1K39ty6o1sHwwlZwO6a7wK",
+        "replacement": "spotify:track:27exgla7YBw9DUNNcTIpjy",
         "originals": [
             // Taylor's Version Pre-release Single
             "spotify:track:1Ov37jtRQ2YNAe8HzfczkL",
@@ -152,11 +168,13 @@ const album_1989 = [
             "spotify:track:22C0JIVhFaczZ4t9heqREN",
             // BMRRS
             "spotify:track:3ScVaMDQwaD8iumBYzcDvr",
+            // 1989 TV standard
+            "spotify:track:1K39ty6o1sHwwlZwO6a7wK"
         ],
     },
     // How You Get The Girl
     {
-        "replacement": "spotify:track:75W3SngKzTuoQ94uLf3y82",
+        "replacement": "spotify:track:733OhaXQIHY7BKtY3vnSkn",
         "originals": [
             // original
             "spotify:track:5Z9ocF6ng0m2haHnKtHwTp",
@@ -166,11 +184,13 @@ const album_1989 = [
             "spotify:track:1l6KjnsqP2atawKgBV49u3",
             // Rep Surprise Playlist
             "spotify:track:3aSYZ4upQ6OSNos26YeVWL",
+            // 1989 TV standard
+            "spotify:track:75W3SngKzTuoQ94uLf3y82"
         ],
     },
     // This Love
     {
-        "replacement": "spotify:track:5QVVjX0ZItqlVpEuVCM9Yg",
+        "replacement": "spotify:track:4WBEj8TeGtRPNJdOmT3WJW",
         "originals": [
             // Taylor's Version Pre-release Single
             "spotify:track:4d1CG5ei1E2vGbvmgf5KKv",
@@ -188,11 +208,13 @@ const album_1989 = [
             "spotify:track:64KRxmbqypPBlya4nxFmDn",
             // BMRRS
             "spotify:track:5yCRSEPJXzjzl3Gov0YduA",
+            // 1989 TV standard
+            "spotify:track:5QVVjX0ZItqlVpEuVCM9Yg"
         ],
     },
     // I Know Places
     {
-        "replacement": "spotify:track:2f0GI2ZLUtbGqFx8t2Gk6A",
+        "replacement": "spotify:track:1ULabO0PEsdJekqVH6G10G",
         "originals": [
             // original
             "spotify:track:6Ck9WerfsyvC4v4AylUoDp",
@@ -202,11 +224,13 @@ const album_1989 = [
             "spotify:track:1fDfzSgIEFWDuX383vRiAv",
             // Rep Surprise Playlist
             "spotify:track:3qGLcmO0XnnrcpAQGZwTvA",
+            // 1989 TV standard
+            "spotify:track:2f0GI2ZLUtbGqFx8t2Gk6A"
         ],
     },
     // Clean
     {
-        "replacement": "spotify:track:2ByBBvpR9b7IynvRmnG7kG",
+        "replacement": "spotify:track:0lKUBmEyQfzsQHozyeXzES",
         "originals": [
             // original
             "spotify:track:2k43eySbfpDVx0PrO99UI8",
@@ -214,34 +238,50 @@ const album_1989 = [
             "spotify:track:1NmVZsG18CzCAtw7rnV3yA",
             // BMRRS
             "spotify:track:1s4CiRufJUyeyQT6HRmu7S",
+            // 1989 TV standard
+            "spotify:track:2ByBBvpR9b7IynvRmnG7kG"
         ],
     },
     // Wonderland
     {
-        "replacement": "spotify:track:2ve0kuaWUCt4Zx8x4bf1MR",
+        "replacement": "spotify:track:6HRsJu8vcnzYDN4t0570FY",
         "originals": [
             // original deluxe
             "spotify:track:6RvRzl1YJTDnUvdOtV21IK",
             // Others
-            "spotify:track:0eTCaVOYLpJlSkbEi2jay4"
+            "spotify:track:0eTCaVOYLpJlSkbEi2jay4",
+            // 1989 TV standard
+            "spotify:track:2ve0kuaWUCt4Zx8x4bf1MR"
         ],
     },
     // You Are In Love
     {
-        "replacement": "spotify:track:4UwqOWDpdeIDVQDuKT6iza",
+        "replacement": "spotify:track:0TyGh27YQ5LknmiDhCzJiT",
         "originals": [
             // original deluxe
             "spotify:track:2KrOAg6FftbjgSKdd2a4rS",
             // Others
-            "spotify:track:5CMw15fx1o2OW2jzaYy36L"
+            "spotify:track:5CMw15fx1o2OW2jzaYy36L",
+            // 1989 TV standard
+            "spotify:track:4UwqOWDpdeIDVQDuKT6iza"
         ],
     },
     // New Romantics
     {
-        "replacement": "spotify:track:2vPMoMDXxu9uX1igWZmXSG",
+        "replacement": "spotify:track:5M787RexsAiVYjQusM98CV",
         "originals": [
             // original deluxe
-            "spotify:track:0qAIiGFKLdV1xpNlEhjpq8"
+            "spotify:track:0qAIiGFKLdV1xpNlEhjpq8",
+            // 1989 TV standard
+            "spotify:track:2vPMoMDXxu9uX1igWZmXSG"
+        ],
+    },
+    // Bad Blook ft. kendrick
+    {
+        "replacement": "spotify:track:6qAcApH8obo8eqatCKUHd9",
+        "originals": [
+            // Bad blood single
+            "spotify:track:6xsEAm6w9oMQYYg3jkEkMT",
         ],
     },
 ]
